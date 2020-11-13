f58fc33501d7e526185735e007c1b7a030c092a2
Loh Jing Zhi <jingzhi.loh@skymind.my>
2020-11-13 17:14:29 +0800

    first commit

---
+++ golden_cross_strategy/run.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import backtrader as bt
+from strategies.GoldenCross import GoldenCross
+from strategies.BuyHold import BuyHold
+import backtrader.analyzers as btanalyzers
+
+cerebro = bt.Cerebro()
+cerebro.broker.set_cash(100000)
+spy_prices = pd.read_csv('data/SPY.csv', index_col='Date', parse_dates=True)
+feed = bt.feeds.PandasData(dataname=spy_prices)
+cerebro.adddata(feed)
+
+cerebro.addstrategy(GoldenCross)
+# cerebro.addstrategy(BuyHold)
+
+cerebro.addanalyzer(btanalyzers.SharpeRatio, _name = "sharpe")
+cerebro.addanalyzer(btanalyzers.Transactions, _name = "transactions")
+cerebro.addanalyzer(btanalyzers.TradeAnalyzer, _name = "trades")
+
+back = cerebro.run()
+cerebro.plot()
+
+cerebro.broker.getvalue()
+sharp = back[0].analyzers.sharpe.get_analysis()
+transactions = back[0].analyzers.transactions.get_analysis()
+trades = back[0].analyzers.trades.get_analysis()
+
+print("#"*20)
+print(f"SharpeRatio: {sharp['sharperatio']}")
+
+
+
